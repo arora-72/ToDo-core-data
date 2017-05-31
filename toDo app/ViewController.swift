76df523102f54286b98a679c06ebb523d1c09882
arora_72 <indresh.arora2016@vitstudent.ac.in>
2017-05-31 18:52:17 +0530

    basic functionality along with core data done

--- toDo app/ViewController.swift
+++ toDo app/ViewController.swift
@@ -8,18 +8,92 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var tasks: [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //get the data from core data
+        fetchData()
+        
+        //reload the table view
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let task = tasks[indexPath.row]
+        
+        if task.isImportant{
+            
+            cell.textLabel?.text = "\(task.name!)"
+        }else{
+        
+            cell.textLabel?.text = task.name!
+        }
+        
+        
+        return cell
+    }
+    
+    
+    func fetchData(){
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        
+        do{
+        tasks = try context.fetch(Task.fetchRequest())
+        }catch{
+            
+            //handle exception
+            print("fetching failed")
+        }
+    }
+  
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        if editingStyle == .delete{
+            let task = tasks[indexPath.row]
+            context.delete(task)
+            
+            //saving the context
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            do{
+            tasks = try context.fetch(Task.fetchRequest())
+            }
+            catch{
+                print("error in deleting")
+            }
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    
 
 }
 
